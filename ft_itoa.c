1fe20b97245c030ad7d08672b19b402e9237f300
shepani-hash <sdof3534@gmail.com>
2024-11-09 18:58:28 +0100

    Sat Nov  9 18:58:28 +01 2024

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,33 +1,52 @@
-#include "libft.h"
-int	ft_strlen(const char	*s)
-{
-	int i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-char *ft_itoa(int n)
-	size_t	i;
-	char    *sign;
-	size_t	r_v;
-
-	i = 0;
-	sign = malloc(ft_strlen(n)*sizeof(int) + 1);
-	r_v = 0;
-	while (n >= 0 && n <= 9)
-	{
-		r_v = r_v * 10 + (n - 48);
-		i++;
-	}
-	return (r_v * sign);
-}
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char *ft_itoa(int n)
+{
+    int sign;
+    int i, n1;
+    char *t;
+
+    n1 = n;
+    if (n < 0)
+    {
+        n *= -1;
+        sign = 1;
+        i++;
+    }
+    while (n1 != 0)
+    {
+        n1 /= 10;
+        i++; 
+    }
+    n1 = i;
+    t = malloc(i+1);
+    if (!t)
+        return (NULL);
+    i = n1 - 1;
+    while (i >= 0)
+    {
+        if (sign == 1)
+        {
+            t[i--] = (n % 10) + '0';
+            t[0] = '-';
+        }
+        else if (sign != 1)
+        {
+            printf("n = %d\n i = %d\n", n, i);    
+            t[i--] = (n % 10) + '0';
+            printf("n = %d\n i = %d\n", n, i);
+        }
+        n /= 10;
+    }
+    t[n1] = '\0';
+    return (t);
+}
+
 int main()
 {
-	int	i = 122; // handel this case
-	ft_itoa(str);
-	printf("%d", ft_itoa(str));// u need understood what happen whit memory
+    int i;
+    i = 123;
+    printf("%s", ft_itoa(-1203));
+
 }
